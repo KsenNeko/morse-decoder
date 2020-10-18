e8ddcd9268b07fc35c43174852e40646fe7c4d96
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2020-10-18 09:55:53 +0400

    feat: decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  expr = expr.replace(new RegExp("11", "g"), "-");
+  expr = expr.replace(new RegExp("10", "g"), ".");
+  for (let i = 0; i < expr.length; i += 11) {
+    expr = expr.replace("**********", " ");
+  }
+  for (let i = 0; i < expr.length; i++) {
+    if (
+      expr[i] === "0" &&
+      expr[i + 1] !== "0" &&
+      expr[i + 1] !== "1" &&
+      expr[i + 1] !== " "
+    ) {
+      let key = "";
+      let j = i;
+      while (
+        expr[j + 1] !== "0" &&
+        expr[j + 1] !== "1" &&
+        expr[j + 1] !== " " &&
+        j + 1 < expr.length
+      ) {
+        key += expr[j + 1];
+        j++;
+      }
+      expr = expr.replace(key, MORSE_TABLE[key]);
+    }
+  }
+  expr = expr.replace(new RegExp("0", "g"), "");
+  return expr;
 }
 
 module.exports = {
